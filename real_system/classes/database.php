70db64a8e50af65ce4d4e8027b2ec0b8b7f041c7
Thien Nguyen <tnguyen@ghost-3.default>
2014-10-25 10:33:46 +0100

    updated database class

---
+++ real_system/classes/database.php
@@ -0,0 +1,55 @@
+<?php
+class database {
+  // A function which creates a connection to the databse
+  public function initiate() {
+    $user             = "thien";
+    $password         = "JmuVnBrLxqAFTEhA";
+    $hostname         = "localhost";
+    $dbn              = "booking";
+  
+    try
+    {
+      $this->database = new PDO("mysql:host={$hostname};dbname={$dbn}", $user, $password);
+    }
+    catch(PDOException $e)
+    { 
+      $error = "I'm unable to connect to the database server.";
+      die("Failed to connect to database: " . $e->getMessage());
+    }
+  
+    $this->database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  }
+
+  // A function which runs a query passed as parameters,
+  // if it fails, email the webmaster and redirect the
+  // user to the oops page
+  public function DoQuery($query, $query_params) {
+    try
+    {
+      $this->result = $this->database->prepare($query);
+      if ($query_params != null)
+      {
+        $this->result->execute($query_params);
+      }
+      else
+      {
+        $this->result->execute();
+      }
+    }
+    catch(PDOException $e)
+    {
+      die();
+    }
+  }
+
+  // A function to fetch a single row from the query
+  public function fetch() {
+    return $this->result->fetch();
+  }
+
+  // A function to fetch multiple rows from the query
+  public function fetchAll() {
+    return $this->result->fetchAll();
+  }
+}
+?>
